ba0518fc15ad600cfc90d706ff7585e1d77ca0c3
Andrei Stoyanow <astoyanow13@gmail.com>
2019-10-31 23:18:23 -0500

    trying to use ajax to connect APIs but getting CORS errors

--- csci340/labs/lab7/script.js
+++ csci340/labs/lab7/script.js
@@ -1,6 +1,50 @@
 $(document).ready(function(){
-
-
-
-
-})
+  $("#shibe-chooser-1").click(function(){
+    $.ajax({
+      crossorigin: true,
+      dataType: "json",
+      url: "http://shibe.online/api/shibes?count=1&urls=true&httpsUrls=true",
+      success: function(results){
+        $("#image1").attr("src",results["image url 1"]);
+      },
+      error: function(xhr,status,error){
+        console.log(error);
+      }
+    });
+    $.ajax({
+      crossorigin: true,
+      dataType: "json",
+      url:"https://evilinsult.com/generate_insult.php?lang=en&type=json",
+      success: function(results){
+        $("#insult1").text(results["insult"]);
+      },
+      error: function(xhr,status,error){
+        console.log(error);
+      }
+    });
+  });
+  $("#shibe-chooser-2").click(function(){
+    $.ajax({
+      crossorigin: true,
+      dataType: "json",
+      url: "http://shibe.online/api/shibes?count=1&urls=true&httpsUrls=true",
+      success: function(results){
+        $("#image2").attr("src",results["image url 1"]);
+      },
+      error: function(xhr,status,error){
+        console.log(error);
+      }
+    });
+    $.ajax({
+      crossorigin: true,
+      dataType: "json",
+      url:"https://evilinsult.com/generate_insult.php?lang=en&type=json",
+      success: function(results){
+        $("#insult2").text(results["insult"]);
+      },
+      error: function(xhr,status,error){
+        console.log(error);
+      }
+    });
+  });
+});
